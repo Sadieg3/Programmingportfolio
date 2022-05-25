c325fd6daad683b01a27a55cc3294ef54d95e0f2
Sadieg3 <89169471+Sadieg3@users.noreply.github.com>
2022-05-25 16:23:47 -0600

    Add files via upload

---
+++ src/99Bottles/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+public static void main(String args[]) {
+int bottles = 99;
+while(bottles > 1) {
+System.out.print(+ bottles);
+System.out.print(" bottles of root beer on the wall ");
+System.out.print(+ bottles);
+System.out.print(" bottles of root beer ");
+bottles--;
+System.out.print("take one down pass it around ");
+System.out.print(+ bottles);
+System.out.print("bottles of  root beer on the wall ");
+System.out.print("\n");
+}
+if (bottles > 0) {
+System.out.print("1 bottle of root beer on the wall 1 bottle of root beer take one down pass it around no more bottles of root beer on the wall");
+}
+}
+}
+
+ 
+
